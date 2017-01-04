a15b23227c5b3657ba53c8cf62f4147b158b7563
raaarwwwwandy <hirandysan@gmail.com>
2017-01-04 01:22:19 -1000

    server is connected, attempting to have it post something

--- server.js
+++ server.js
@@ -29,13 +29,13 @@ const server = http.createServer( (req, res) => {
   if(req.url === "/"){
     req.url = "/index.html";
   }
-  console.log("req.url", req.url);
-  console.log("req.method", req.method);
-  console.log('req.headers', req.headers);
+  // console.log("req.url", req.url);
+  // console.log("req.method", req.method);
+  // console.log('req.headers', req.headers);
+  // console.log("req.method", req);
 
 
-
-
+ if(req.method === "GET"){
  if(resourceMapping.hasOwnProperty(req.url) ){
   fs.readFile(resourceMapping[req.url], (err, content) =>{
     if(err) {
@@ -48,22 +48,36 @@ const server = http.createServer( (req, res) => {
   res.setstatuscode = 404;
   sendContent(res, 'No stay brah');
   return;
+  }
  }
 
 
+ if(req.method === "POST"){
+  if(req.url === resourceMapping.req.url){
+    fs.readFile(resourceMapping[req.url]);
+  } else{
+    fs.writeFile(`${req.url}`, 'data',(err, chunk) =>{
+let reqBody = " ";
+  req.setEncoding('utf8');
+  req.on('data', (chunk) => {
+    reqBody += chunk;
+    let splitChunk = reqBody.split('&');
+    console.log(splitChunk);
+  });
+  req.on('end', () =>{
+    // console.log(`what is reqBody: ${reqBody}`);
+  });
+    });
+
+  
+ }
+}
   
 
 });
 
-// let reqBody = " ";
-//   req.setEncoding('utf8');
-//   req.on('data', (chunk) => {
-//     reqBody += chunk;
-//   });
-//   req.on('end', () => {
-//     console.log(reqBody);
-//   });
-  
+
+
 server.listen(PORT, () => {
   console.log("server is listening on port", PORT);
 });
